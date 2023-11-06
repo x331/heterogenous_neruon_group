88e2953cc9dff06ba62763fead3319a2af3e5ad1
x331 <89317804+x331@users.noreply.github.com>
2023-11-06 17:14:40 -0500

    why

--- quicknets-master/early_exit/vgg/train.py
+++ quicknets-master/early_exit/vgg/train.py
@@ -9,6 +9,7 @@ import sys
 import time
 
 sys.path.append('..')
+print('hello')
 from datasets import IndexedCIFAR10, IndexedCIFAR100
 from train_utils import train_decision_layer, train_epoch, train_epoch_joint_loss, get_minimal_trained_indices
 from torchvision import datasets, transforms
