c46a2d4e3997276e3293b7d166207d511c5faa83
x331 <89317804+x331@users.noreply.github.com>
2023-11-06 17:18:54 -0500

    wy

--- quicknets-master/early_exit/vgg/train.py
+++ quicknets-master/early_exit/vgg/train.py
@@ -8,8 +8,8 @@ import numpy as np
 import sys
 import time
 
-sys.path.append('..')
-print('hello')
+sys.path.append('../')
+print(sys.path)
 from datasets import IndexedCIFAR10, IndexedCIFAR100
 from train_utils import train_decision_layer, train_epoch, train_epoch_joint_loss, get_minimal_trained_indices
 from torchvision import datasets, transforms
