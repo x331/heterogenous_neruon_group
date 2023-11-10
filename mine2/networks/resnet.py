0f49313b0b113422e973a14808bbdcb17d89a9f0
x331 <89317804+x331@users.noreply.github.com>
2023-11-10 16:18:07 -0500

    w

--- mine2/networks/resnet.py
+++ mine2/networks/resnet.py
@@ -251,6 +251,18 @@ class InfoProResNet(nn.Module):
                             loss.backward()
                             x = x.detach()
                             local_module_i += 1
+                            
+                    if self.joint_train:
+                        print('hi')
+                        if self.infopro_config[local_module_i][0] == stage_i \
+                                and self.infopro_config[local_module_i][1] == layer_i:
+                            loss_ixx = eval('self.decoder_' + str(stage_i) + '_' + str(layer_i))(x, self._image_restore(img))
+                            loss_ixy = eval('self.aux_classifier_' + str(stage_i) + '_' + str(layer_i))(x, target)
+                            print(loss_ixx,loss_ixx,target)
+                            loss = ixx_r * loss_ixx + ixy_r * loss_ixy
+                            loss.backward()
+                            x = x.detach()
+                            local_module_i += 1
 
             x = self.avgpool(x)
             x = x.view(x.size(0), -1)
