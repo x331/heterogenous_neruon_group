8035873d6ecf1ad775d4ccfb66472d6e8a6a3a83
x331 <89317804+x331@users.noreply.github.com>
2023-11-10 21:16:41 -0500

    w

--- mine2/utils.py
+++ mine2/utils.py
@@ -3,9 +3,9 @@ import torch
 #input a list of tensors with each tensor representing an exit loss
 def early_exit_joint_loss(losses,wieghts=0):
     if wieghts==0:
-        return torch.mean(torck.vstack(losses))
+        return torch.mean(torch.vstack(losses))
     if wieghts==1:
-        return torch.mean(torck.vstack(losses))
+        return torch.mean(torch.vstack(losses))
         
     
     
