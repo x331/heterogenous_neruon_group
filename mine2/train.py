779c2e813126f730b2343d7b716332b6182df03b
x331 <89317804+x331@users.noreply.github.com>
2023-11-11 00:56:42 -0500

    o

--- mine2/train.py
+++ mine2/train.py
@@ -494,7 +494,8 @@ def accuracy(output, target, topk=(1,)):
     res = []
     for k in topk:
         print(correct.shape)
-        correct_k = correct[:k].view(-1).float().sum(0)
+        # correct_k = correct[:k].view(-1).float().sum(0)
+        correct_k = correct[:k].flatten().float().sum(0)
         print(correct_k)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
