a7e1a29ae5dacca6e87d00b8e7263232ea920749
x331 <89317804+x331@users.noreply.github.com>
2023-11-11 00:52:27 -0500

    d

--- mine2/train.py
+++ mine2/train.py
@@ -339,7 +339,7 @@ def train(train_loader, model, optimizer, epoch):
 
         # measure accuracy and record loss
         if args.local_module_num ==1:
-            prec1 = accuracy(output[0].data, target, topk=(1,))[0]
+            prec1 = accuracy(output[0].data, target, topk=(1,5))[0]
             losses.update(loss.data.item(), x.size(0))
             top1.update(prec1.item(), x.size(0))
         else:
