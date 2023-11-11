792e7c93ff45daeec3ea83456063739cb6e442d3
x331 <89317804+x331@users.noreply.github.com>
2023-11-11 00:23:04 -0500

    m

--- mine2/train.py
+++ mine2/train.py
@@ -501,8 +501,7 @@ def accuracy_all_exits(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
     batch_size = target.size(0)
-    print(output)
-    print(torch.vstack(output).shape)
+    print(torch.stack(output).shape)
 
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
