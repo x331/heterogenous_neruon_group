0fde766a2d002d13d045b23b0543d51994a2a9d4
x331 <89317804+x331@users.noreply.github.com>
2023-11-13 00:03:03 -0500

    w

--- mine2/runme.sh
+++ mine2/runme.sh
@@ -1,1 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python train.py --dataset cifar10 --model resnet --layers 32 --droprate 0.0 --cos_lr --local_module_num 16  --joint_train --local_loss_mode cross_entropy --train_total_epochs 500 --aux_net_widen 1 --aux_net_feature_dim 128  --aux_net_config 1c2f
+
+CUDA_VISIBLE_DEVICES=0 python /train.py --layerwise_train --local_module_num 16 --train_total_epochs 3200 --dataset cifar10 --model resnet --layers 32 --droprate 0.0 --cos_lr   --local_loss_mode cross_entropy --aux_net_widen 1 --aux_net_feature_dim 128  --print-freq 1  --aux_net_config 1c2f
