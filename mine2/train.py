924fdc22c7be1dc199b348d4985b8f93800162a2
x331 <89317804+x331@users.noreply.github.com>
2023-11-13 12:43:46 -0500

    w

--- mine2/train.py
+++ mine2/train.py
@@ -606,7 +606,7 @@ def accuracy_all_exits(output, target, topk=(1,)):
     p = ((1/(np.log(output.shape[2])))* (prob*torch.log(prob)).sum(dim=2)).cpu()
     p = p+1
     print(torch.max(p),torch.min(p))
-    e = p>0.005
+    e = p>0.003
     exits = torch.zeros(p.shape[0],1,device='cpu')
     exits_acc = torch.zeros(p.shape[0],1,device='cpu')
     for m in range(p.shape[0]):
