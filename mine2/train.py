650944ce211720db05fc2c9a6e519b2672ee34a4
x331 <89317804+x331@users.noreply.github.com>
2023-11-13 11:35:34 -0500

    w

--- mine2/train.py
+++ mine2/train.py
@@ -607,8 +607,9 @@ def accuracy_all_exits(output, target, topk=(1,)):
     # print((prob*torch.log(prob)).sum(dim=2,keepdim=True).shape)
     p = (1/(np.log(output.shape[2])))* (prob*torch.log(prob)).sum(dim=2,keepdim=True)
     print(p.shape)
-    print((torch.stack([p>.7])).shape)
-    print((p[p>.7].sum(dim=1,keepdim=True)).shape)
+    print([p>.7][0].shape)
+    print(p[p>.7].shape)
+    print(p[p>.7].sum().shape())
     print(p[p>.7].sum(dim=1,keepdim=True))
     
     pred = pred.reshape(pred.shape[0],pred.shape[2],pred.shape[1])
