26033b4a62b03690d1d5c38851542ad80386445d
x331 <89317804+x331@users.noreply.github.com>
2023-11-13 18:22:22 -0500

    w

--- mine2/networks/resnet.py
+++ mine2/networks/resnet.py
@@ -285,6 +285,9 @@ class InfoProResNet(nn.Module):
                 for layer_i in range(self.layers[stage_i - 1]):
                     x = eval('self.layer' + str(stage_i))[layer_i](x)
                     print(eval('self.layer' + str(stage_i))[layer_i].requires_grad_)
+                    eval('self.layer' + str(stage_i))[layer_i].requires_grad_ = False
+                    print(eval('self.layer' + str(stage_i))[layer_i].requires_grad_)
+
 
                     if local_module_i <= self.local_module_num - 2:
                         if self.infopro_config[local_module_i][0] == stage_i \
