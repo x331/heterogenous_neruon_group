4ca1314b60d37d08896fa2c4cfb1e9f22f40f9ae
x331 <89317804+x331@users.noreply.github.com>
2023-11-13 13:17:35 -0500

    w

--- mine2/train.py
+++ mine2/train.py
@@ -504,7 +504,7 @@ def validate(val_loader, model, epoch):
         for idx, meter in enumerate(per_exit_loss_meter):
             meter.update(per_exit_loss[idx].item(), input.size(0)) 
             
-        exit_num , exit_acc = accuracy_all_exits_exit_accuracy(output, target, topk=(1,),args.confidence_threshold)
+        exit_num , exit_acc = accuracy_all_exits_exit_accuracy(output, target, topk=(1,), threshold=args.confidence_threshold)
         for idx, meter in enumerate(per_exit_number_of_exits_meter):
             meter.update(exit_num[idx].item(), x.size(0))  
         for idx, meter in enumerate(per_exit_acc_when_exit_meter):
