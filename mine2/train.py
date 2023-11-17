ab8cb834dd2db43f06bd7dc359b6f48714ea6922
samonuall <samonuall@gmail.com>
2023-11-17 14:22:03 -0500

    add

--- mine2/train.py
+++ mine2/train.py
@@ -310,7 +310,8 @@ def main():
             if epoch % epochs_per_module == 0:
                 curr_module += 1
                 freeze_modules_before(model.module, curr_module)
-                print("Model: ", model.named_parameters.keys())
+                for name, param in model.named_parameters():
+                    print(name)
                 optimizer = torch.optim.SGD([param for param in model.parameters() if param.requires_grad],
                                     lr=training_configurations[args.model]['initial_learning_rate'],
                                     momentum=training_configurations[args.model]['momentum'],
