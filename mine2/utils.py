387bb4036ed3237d75554c2fa617e9ad945593c4
samonuall <samonuall@gmail.com>
2023-11-17 12:48:06 -0500

    go back to original

--- mine2/utils.py
+++ mine2/utils.py
@@ -9,25 +9,23 @@ def early_exit_joint_loss(losses,wieghts=0):
         
     
 
-def freeze_module_before(model, target_module):
+def freeze_modules_before(model, target_module):
     if target_module > 0:
+        curr_module = 0
         print("Starting module freeze")
-        stage = model.infopro_config[target_module - 1][0]
-        # layer right after the end of the previous module
-        if target_module > 1:
-            start_layer = model.infopro_config[target_module - 2][1] + 1
-        else:
-            start_layer = 0
-        
-        for layer_i in range(start_layer, model.layers[stage - 1]):
-            # freeze layer
-            eval('model.layer' + str(stage))[layer_i].requires_grad = False
-            
-            # reached end of module
-            if model.infopro_config[target_module - 1][1] == layer_i:
-                eval('model.aux_classifier_' + str(stage) + '_' + str(layer_i)).requires_grad = False
-                print("Finished module freeze")
-                return
+        for stage_i in (1, 2, 3):
+            for layer_i in range(model.layers[stage_i - 1]):
+                # freeze layer
+                eval('model.layer' + str(stage_i))[layer_i].requires_grad = False
 
-        print("Froze everything")
-        
+                # reached end of module
+                if model.infopro_config[curr_module][0] == stage_i \
+                    and model.infopro_config[curr_module][1] == layer_i:
+                    print("Froze module " + str(curr_module))
+                    eval('model.aux_classifier_' + str(stage_i) + '_' + str(layer_i)).requires_grad = False
+                    curr_module += 1
+                    if curr_module == target_module:
+                        print("Finished gradient freeze")
+                        return
+
+    print("Froze everything")
