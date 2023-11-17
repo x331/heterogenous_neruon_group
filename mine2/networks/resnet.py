8fa069115f263e8b79f5cf4c3fecc26bbb081356
samonuall <samonuall@gmail.com>
2023-11-16 20:49:25 -0500

    remove print

--- mine2/networks/resnet.py
+++ mine2/networks/resnet.py
@@ -257,7 +257,6 @@ class InfoProResNet(nn.Module):
             for stage_i in (1, 2, 3):
                 for layer_i in range(self.layers[stage_i - 1]):
                     x = eval('self.layer' + str(stage_i))[layer_i](x)
-                    print("[[[ current layer:", eval('self.layer' + str(stage_i))[layer_i])
 
                     if local_module_i <= self.local_module_num - 2:
                         if self.infopro_config[local_module_i][0] == stage_i \
