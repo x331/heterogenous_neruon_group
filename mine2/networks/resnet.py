d80304bc3ae13517d3439db05864e82c80ff5c1e
x331 <89317804+x331@users.noreply.github.com>
2023-11-29 00:34:37 -0500

    w

--- mine2/networks/resnet.py
+++ mine2/networks/resnet.py
@@ -56,9 +56,10 @@ class BasicBlock(nn.Module):
             self.conv2b = conv3x3(num_chan2, num_chan2, stride)    
             if  downsample != None:
                 if self.beginning:
+                    print(downsample,self.expansion)
                     self.downsample = nn.Sequential(
-                        nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                        nn.BatchNorm2d(planes * block.expansion)
+                        nn.Conv2d(inplanes, planes * downsample, kernel_size=1, stride=stride, bias=False),
+                        nn.BatchNorm2d(planes * downsample)
                     )
                 else:
                     in_chan1 = math.floor(inplanes*h_ratio)
