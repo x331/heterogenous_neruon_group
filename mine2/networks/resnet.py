d5557db9e0da8ff314c8d16317e6f15e28c07d24
x331 <89317804+x331@users.noreply.github.com>
2023-12-05 15:34:23 -0500

    h splitting

--- mine2/networks/resnet.py
+++ mine2/networks/resnet.py
@@ -563,8 +563,8 @@ class InfoProResNet(nn.Module):
                             and self.infopro_config[local_module_i][1] == layer_i:
                         
                         chan_1 = math.floor(self.h_split_ratios[self._get_local_mod_pos(stage_i,layer_i)]*x.shape[0])
-                        xa_a = x[:chan_1]
-                        xb_a = x[chan_1:]
+                        xa_a = x[:,:chan_1]
+                        xb_a = x[:,chan_1:]
                         xa_d = xa_a.detach()
                         xb_d = xb_a.detach()
                         xa = torch.cat((xa_a,xb_d),dim=1)
